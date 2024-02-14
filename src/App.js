d8abe9c644402d59199127d8877e99cad5dc685b
foolish coder <122173165+foolishcoder97@users.noreply.github.com>
2024-02-15 01:44:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Randomgif from './Randomgif';
+function App() {
+  return (
+    <div className="App">
+      <h1 className='page-heading'>Random GIF Generator</h1>
+      
+      <Randomgif/>
+    </div>
+  );
+}
+
+export default App;
